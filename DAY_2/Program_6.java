63c5e1e8383241a15db32b653ebbf09d101a245f
RiddheshKumar <kumar123riddhesh@gmail.com>
2026-05-29 18:20:21 +0530

    DAY_2 <br> adding Program_6

---
+++ DAY_2/Program_6.java
@@ -0,0 +1,16 @@
+package DAY_2;
+//WAP to reverse a number.
+import java.util.Scanner;
+public class Program_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int ld = 0;
+        while(n>0){
+            ld = n%10;
+        }
+
+    }
+
+}
