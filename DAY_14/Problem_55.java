771148a8a7aaa1d7914c81afa179a37b8816482d
RiddheshKumar <kumar123riddhesh@gmail.com>
2026-06-23 20:53:41 +0530

    Adding Solution of DAY 14 Problems.

--- DAY_14/Problem_55.java
+++ DAY_14/Problem_55.java
@@ -1,31 +1,29 @@
 package DAY_14;
-//Write a program to Second largest element.
 import java.util.Scanner;
 public class Problem_55 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of the Array: ");
+        System.out.println("Enter Length of the array: ");
         int n = sc.nextInt();
         int [] arr = new int[n];
-        System.out.println("Enter Element of the array: ");
-        for (int i = 0 ; i<n;i++){
+        System.out.println("Enter Elements of the array: ");
+        for (int i = 0;i<n;i++){
             arr[i] = sc.nextInt();
         }
         int max = arr[0];
-        int max2 = arr[0];
-        for (int i = 1;i<n;i++){
+        int max_2 = arr[0];
+
+        for (int i = 0;i<n;i++){
             if (arr[i]>max){
-                max2 = max;
                 max = arr[i];
             }
-            else
-            {
-                if (arr[i]>max2){
-                    max2 = arr[i];
-                }
+        }
+        System.out.println("Max element is "+max);
+        for (int i = 0;i<n;i++){
+            if (arr[i]>max_2 && arr[i]!=max){
+                max_2 = arr[i];
             }
         }
-        System.out.println("Largest Element of the array is: "+max);
-        System.out.println("2nd largest Element of the array is: "+max2);
+        System.out.println("2nd Max element is "+max_2);
     }
 }
